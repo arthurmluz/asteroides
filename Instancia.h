bd41c7a008456b42724489bf4c4ae496a1706837
Arthur Luz <arthurmluz123@gmail.com>
2022-05-07 17:52:04 -0300

    Commit inicial

---
+++ Instancia.h
@@ -0,0 +1,33 @@
+//
+//  Instancia.hpp
+//  OpenGLTest
+//
+//  Created by Márcio Sarroglia Pinho on 22/09/20.
+//  Copyright © 2020 Márcio Sarroglia Pinho. All rights reserved.
+//
+
+
+#ifndef Instancia_hpp
+#define Instancia_hpp
+#include <iostream>
+using namespace std;
+
+#include "Poligono.h"
+typedef void TipoFuncao();
+
+class Instancia{
+public:
+    //Poligono *modelo;
+    int nroCurva;
+    float tAtual;
+    float deltaT = 1.0/50;
+    Instancia();
+    TipoFuncao *modelo;
+    Ponto posicao, escala;
+    float rotacao;
+    void desenha();
+    void setModelo(Poligono *P);
+};
+
+
+#endif /* Instancia_hpp */
